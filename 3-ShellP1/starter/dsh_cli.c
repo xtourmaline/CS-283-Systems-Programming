503c4ffb6d356368a2330e607c895ea388a79bf6
Vanessa Trinh <xtourmaline@gmail.com>
2025-02-08 20:45:11 -0500

    implemented exit

--- 3-ShellP1/starter/dsh_cli.c
+++ 3-ShellP1/starter/dsh_cli.c
@@ -46,10 +46,24 @@
  */
 int main()
 {
-    char *cmd_buff;
+    char cmd_buff[ARG_MAX];
     int rc = 0;
     command_list_t clist;
 
-    printf(M_NOT_IMPL);
-    exit(EXIT_NOT_IMPL);
+    while (1)
+    {
+        printf("%s", SH_PROMPT);
+        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL)
+        {
+            printf("\n");
+            break;
+        }
+        // remove the trailing \n from cmd_buff
+        cmd_buff[strcspn(cmd_buff, "\n")] = '\0';    
+
+        // IMPLEMENT THE REST OF THE REQUIREMENTS
+        if (strlen(cmd_buff) == 4 && strncmp(EXIT_CMD, cmd_buff, 4) == 0) {
+            exit(0);
+        }
+    }
 }
